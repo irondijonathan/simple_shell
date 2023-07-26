4f669a5c04c4b519671362e6e3797a3a8ea03c9a
irondijonathan <irondicjonathan@gmail.com>
2023-07-26 07:25:36 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+typedef struct
+{
+	char *line;
+	char **args;
+} shell_t;
+
+void free_shell_data(shell_t *shell_data);
+
+extern char **environ;
+
+void print_environ(void);
+
+void free_string_array(char *args[]);
+
+int _execute_command(char *args[]);
+
+void _print_error(char *sn, char *command);
+
+int _strlen(char *s);
+
+int _strcmp(char *s1, char *s2);
+
+char **tokenize_string(char *s, const char *t);
+
+void remove_newline(char *s);
+
+#endif /* MAIN_H */
